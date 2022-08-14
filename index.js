f05a026a8a15c1b3c5e64ed06fddeaf9204f59b9
CaoTrongThang <fptisthebest@gmail.com>
2022-08-14 19:15:16 +0700

    first calculator

---
+++ index.js
@@ -0,0 +1,15 @@
+let currentInput = document.getElementById("current-input");
+let 
+
+function GetInput(value) {
+    currentInput.value += value;
+}
+
+function Operator(value) {
+  if (value == "+") 
+  {
+    if(){
+
+    }
+  }
+}
